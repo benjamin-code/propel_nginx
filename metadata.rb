577bc514e0c25697bf3e07e64a3e2419b2dc1376
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-01-15 13:39:05 +0800

    Change VIP

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'ecs_python'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Nginx'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
