bb8457667d252936ccf03b2163826fc341ac2941
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-01-25 14:57:18 +0800

    Add FT1 environment

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,24 +1,13 @@
-
-#Production variables:
-default[:propel_nginx][:nginx_for_ui] = ""
+if ( node.hostname =~ /atc(.*)/ )
+  puts "This is the Prod environment."
+#default[:propel_nginx][:nginx_for_ui] = ""
 default[:propel_nginx][:nginx_ha_master] = "atc-cr-iweb6"
 default[:propel_nginx][:nginx_ha_backup] = "atc-cr-iweb7"
 default[:propel_nginx][:propel_ui_1] = "propel-ha-3"
 default[:propel_nginx][:propel_ui_2] = "propel-ha-4"
 default[:propel_nginx][:propel_backend_1] = "propel-ha-3"
 default[:propel_nginx][:propel_backend_2] = "propel-ha-4"
-default[:propel_nginx][:vip] = "30.161.224.210/24" #Need to set VIP of Prod env..
-
-if ( node.hostname =~ /centos6(.*)/ )
-  puts "This is the test environment."
-default[:propel_nginx][:nginx_for_ui] = ""
-default[:propel_nginx][:nginx_ha_master] = "centos6-5"
-default[:propel_nginx][:nginx_ha_backup] = "centos6-6"
-default[:propel_nginx][:propel_ui_1] = "15.107.12.82"
-default[:propel_nginx][:propel_ui_2] = "15.107.12.82"
-default[:propel_nginx][:propel_backend_1] = "15.107.12.82"
-default[:propel_nginx][:propel_backend_2] = "15.107.12.82"
-default[:propel_nginx][:vip] = "15.107.13.122/24"
+#default[:propel_nginx][:vip] = "30.161.224.210/24" #Need to set VIP of Prod env..
 end
 
 if ( node.hostname =~ /propel-ha(.*)/ )
@@ -33,3 +22,14 @@ default[:propel_nginx][:propel_backend_2] = "propel-ha-6"
 default[:propel_nginx][:vip] = "30.161.224.149"
 end
 
+if ( node.hostname =~ /pln(.*)/ )
+  puts "This is the FT1 environment."
+#default[:propel_nginx][:nginx_for_ui] = "propel-ha-1"
+default[:propel_nginx][:nginx_ha_master] = "pln-cd1-iweb8"
+default[:propel_nginx][:nginx_ha_backup] = "pln-cd1-iweb9"
+default[:propel_nginx][:propel_ui_1] = "pln-cd1-eweb8"
+default[:propel_nginx][:propel_ui_2] = "pln-cd1-eweb9"
+default[:propel_nginx][:propel_backend_1] = "pln-cd1-iweb3"
+default[:propel_nginx][:propel_backend_2] = "pln-cd1-iweb4"
+#default[:propel_nginx][:vip] = "204.104.5.84"
+end
