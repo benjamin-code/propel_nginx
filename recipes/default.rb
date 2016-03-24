8577bc50829af7d89fe4a670e59b2aac24bdf46f
Wang Yun <yun.wang2@hpe.com>
2016-03-24 22:35:58 +0800

    modify logs to 644
    Change-Id: I116557edb399d605788b13d961d1da3c2cce50da

--- recipes/default.rb
+++ recipes/default.rb
@@ -97,3 +97,9 @@ template "/etc/nginx/conf.d/propel.conf" do
      only_if { node.chef_environment == 'prod' }
      notifies :restart, "service[nginx]", :immediately
 end
+
+cookbook_file '/etc/logrotate.d/nginx' do
+  source 'logrotate' 
+  mode '0644'
+  notifies :restart, "service[nginx]", :immediately
+end
