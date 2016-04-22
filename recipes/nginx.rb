aeede1b43ff514f242504797a4b6f520785ea5be
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-04-22 13:36:53 +0800

    Seperate sandbox in nginx recipe

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -26,8 +26,19 @@ template "/etc/nginx/conf.d/sm.conf" do
     variables ({
       :upstream_1 => node[:propel_nginx][:backend_1],
       :upstream_2 => node[:propel_nginx][:backend_2],
-#      :upstream_1 => node[:propel_nginx][:backend_3],
-#      :upstream_2 => node[:propel_nginx][:backend_4],
+      :upstream_1 => node[:propel_nginx][:backend_3],
+      :upstream_2 => node[:propel_nginx][:backend_4],
     })
+    only_if "node.chef_environment == 'env1' || node.chef_environment == 'prod'"
+    notifies :restart, "service[nginx]", :immediately
+end
+
+template "/etc/nginx/conf.d/sm.conf" do
+    source "nginx.sm.conf.erb"
+    variables ({
+      :upstream_1 => node[:propel_nginx][:backend_1],
+      :upstream_2 => node[:propel_nginx][:backend_2],
+    })
+    only_if "node.chef_environment == 'sandbox'"
     notifies :restart, "service[nginx]", :immediately
 end
