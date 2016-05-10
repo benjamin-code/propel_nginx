030851a397d35234306c52d46c8691913fdd18da
jun-zhou3 <jun.zhou3@hpe.com>
2016-05-10 16:59:18 +0800

    modified iptables recipes message

--- recipes/iptables_nginx_app.rb
+++ recipes/iptables_nginx_app.rb
@@ -3,7 +3,7 @@ bash 'configure_nginx_app_iptables' do
     for port in 9595 9510 9444 9810 8449 9040 9600 8000 8001 8002 9843; do
     if iptables -L -n | grep -n "dpt.*${port}"
       then
-        echo "existed rule."; 
+        echo "port ${port} already exists in iptables."; 
       else 
         iptables -I INPUT -p tcp -m tcp --dport ${port} -j ACCEPT
       fi;
