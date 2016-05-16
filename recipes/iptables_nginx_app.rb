bbf5112978f5616cd89c57daf3c6515b9acb7881
jun-zhou3 <jun.zhou3@hpe.com>
2016-05-17 04:45:37 +0800

    add new service port to iptables

--- recipes/iptables_nginx_app.rb
+++ recipes/iptables_nginx_app.rb
@@ -1,6 +1,6 @@
 bash 'configure_nginx_app_iptables' do
   code <<-EOH
-    for port in 9595 9510 9444 9810 8449 9040 9600 8000 8001 8002 9843; do
+    for port in 9595 9510 9444 9810 8449 9040 9600 8000 8001 8002 9843 9530 9050; do
     if iptables -L -n | grep -n "dpt.*${port}"
       then
         echo "port ${port} already exists in iptables."; 
