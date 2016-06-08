b5989e9a6def755e1ac381a67af611db28735e35
zuohao.wang@hpe.com <zuohao.wang@hpe.com>
2016-06-08 14:26:39 +0800

    add haproxy

---
+++ recipes/haproxy.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: propel_nginx
+# Recipe:: default
+
+
+package "haproxy" do
+  action :install
+end
+
+template "/etc/default/haproxy" do
+  source "haproxy-default.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+service "haproxy" do
+  supports :restart => true, :status => true, :reload => true
+  action [:enable, :start]
+end
+
+
+template "/etc/haproxy/haproxy.cfg" do
+    source "haproxy.cfg.env1.erb"
+    mode 0644
+    variables ({
+      :app_1 => node[:propel_nginx][:propel_appserver_1],
+      :app_2 => node[:propel_nginx][:propel_appserver_2]
+    })
+        only_if { node.chef_environment == 'env1' }
+        notifies :restart, "service[haproxy]", :immediately
+end
+
+template "/etc/haproxy/haproxy.cfg" do
+    source "haproxy.cfg.erb"
+    mode 0644
+    variables ({
+      :app_1 => node[:propel_nginx][:propel_appserver_1],
+      :app_2 => node[:propel_nginx][:propel_appserver_2],
+      :app_3 => node[:propel_nginx][:propel_appserver_3],
+      :app_4 => node[:propel_nginx][:propel_appserver_4]
+    })
+     only_if { node.chef_environment == 'prod' }
+     notifies :restart, "service[haproxy]", :immediately
+end
